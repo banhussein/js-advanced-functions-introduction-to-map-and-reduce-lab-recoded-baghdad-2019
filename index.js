bf93eec8394e894938a0c07ce70909d139570f01
ban hussein <banhussien97@gmail.com>
2019-10-16 15:54:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Your code here
+  let r = []
+function mapToNegativize(src) {
+
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(-1 * src[i])
+  }
+  return r
+}
+
+function mapToNoChange(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i])
+  }
+}
+
+function mapToDouble(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(2 * src[i])
+  }
+  return r
+}
+
+function mapToSquare(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i] * src[i])
+  }
+  return r
+}
+
+function reduceToTotal(src, startingPoint=0) {
+  let total = startingPoint
+  for (let i = 0; i < src.length; i++ ) {
+    total = total + src[i]
+  }
+  return total
+}
+
+function reduceToAllTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (!src[i]) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (src[i]) return true
+  }
+  return false
+}
